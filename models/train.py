84a53914e16375b0c4bf1bc3bfd0189fcb80b7cf
ByloTonix <bylotonix@gmail.com>
2024-04-01 00:05:42 +0300

    added validation step
    - added validation step

--- models/train.py
+++ models/train.py
@@ -29,3 +29,30 @@ def train(model, train_loader, criterion, optimizer, device, num_epochs):
         accuracies.append(epoch_acc)
         epochs.append(epoch + 1)
     return losses, epochs, accuracies
+
+
+def validate(model, train_loader, criterion, optimizer, device, num_epochs):
+    model.to(device)
+    model.eval()
+    losses, epochs, accuracies = [], [], []
+    with torch.no_grad():
+
+      for epoch in range(num_epochs):
+          running_loss = 0.0
+          correct, total = 0, 0
+          cool_progress_bar = tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}. Validating {model.__class__.__name__}', unit='batch')
+          for inputs, labels in cool_progress_bar:
+              inputs, labels = inputs.to(device), labels.to(device)
+              outputs = model(inputs)
+              loss = criterion(outputs, labels)
+              running_loss += loss.item() * inputs.size(0)
+              _, predicted = torch.max(outputs, 1)
+              total += labels.size(0)
+              correct += (predicted == labels).sum().item()
+              cool_progress_bar.set_postfix(loss=running_loss / len(train_loader.dataset), acc=correct / total)
+          epoch_loss = running_loss / len(train_loader.dataset)
+          epoch_acc = correct / total
+          losses.append(epoch_loss)
+          accuracies.append(epoch_acc)
+          epochs.append(epoch + 1)
+    return losses, epochs, accuracies
