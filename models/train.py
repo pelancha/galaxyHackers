1ece79ff483bd51448c4d8e32712e3b9acefb89d
ByloTonix <bylotonix@gmail.com>
2024-04-03 00:12:06 +0300

    Update train.py

--- models/train.py
+++ models/train.py
@@ -1,13 +1,8 @@
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-
-def train(model, train_loader, criterion, optimizer, device, num_epochs):
+def train(model, train_loader, criterion, optimizer, device, num_epochs, start_epoch=0):
     model.to(device)
     model.train()
     losses, epochs, accuracies = [], [], []
-    for epoch in range(num_epochs):
+    for epoch in range(start_epoch, num_epochs):
         running_loss = 0.0
         correct, total = 0, 0
         cool_progress_bar = tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}. Training {model.__class__.__name__}', unit='batch')
@@ -28,16 +23,27 @@ def train(model, train_loader, criterion, optimizer, device, num_epochs):
         losses.append(epoch_loss)
         accuracies.append(epoch_acc)
         epochs.append(epoch + 1)
+
+        os.makedirs('trained_models/', exist_ok=True)
+        torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'loss': epoch_loss, 'accuracy': epoch_acc}, f'trained_models/{model.__class__.__name__}_epoch_{epoch + 1}.pth')
     return losses, epochs, accuracies
 
+def continue_training(model, train_loader, criterion, optimizer, device, num_epochs, filepath):
+    loaded_model = torch.load(filepath)
+    model.load_state_dict(loaded_model["model_state_dict"])
+    optimizer.load_state_dict(loaded_model["optimizer_state_dict"])
+    start_epoch = loaded_model['epoch'] + 1
 
-def validate(model, train_loader, criterion, optimizer, device, num_epochs):
+    losses, epochs, accuracies = train(model, train_loader, criterion, optimizer, device, num_epochs, start_epoch)
+
+    return losses, epochs, accuracies
+
+def validate(model, train_loader, criterion, device, num_epochs, start_epoch=0):
     model.to(device)
     model.eval()
-    losses, epochs, accuracies = [], [], []
+    val_losses, val_epochs, val_accuracies = [], [], []
     with torch.no_grad():
-
-      for epoch in range(num_epochs):
+      for epoch in range(start_epoch, num_epochs):
           running_loss = 0.0
           correct, total = 0, 0
           cool_progress_bar = tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}. Validating {model.__class__.__name__}', unit='batch')
@@ -50,9 +56,9 @@ def validate(model, train_loader, criterion, optimizer, device, num_epochs):
               total += labels.size(0)
               correct += (predicted == labels).sum().item()
               cool_progress_bar.set_postfix(loss=running_loss / len(train_loader.dataset), acc=correct / total)
-          epoch_loss = running_loss / len(train_loader.dataset)
-          epoch_acc = correct / total
-          losses.append(epoch_loss)
-          accuracies.append(epoch_acc)
-          epochs.append(epoch + 1)
-    return losses, epochs, accuracies
+          val_epoch_loss = running_loss / len(train_loader.dataset)
+          val_epoch_acc = correct / total
+          val_losses.append(val_epoch_loss)
+          val_accuracies.append(val_epoch_acc)
+          val_epochs.append(1)
+    return val_losses, val_epochs, val_accuracies
