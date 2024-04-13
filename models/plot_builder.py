535013ae23f120a8d376a1683d2e904acc92f6c9
ByloTonix <bylotonix@gmail.com>
2024-04-14 00:11:22 +0300

    Fixes + exporting plots in the separate files

--- models/plot_builder.py
+++ models/plot_builder.py
@@ -1,15 +1,10 @@
 import matplotlib.pyplot as plt
-
-# results = {
-#     'ResNet18': {'losses': [1.6998418815188938, 0.42324265739652844], 'epochs': [1, 2]},
-#     'ResNet50': {'losses': [1.689236854256524, 0.3228870271788703], 'epochs': [1, 2]},
-#     'EfficientNet': {'losses': [0.656188617808289, 0.11389627428981992], 'epochs': [1, 2]},
-#     'ViT': {'losses': [0.23428887585136626, 0.10356608468770152], 'epochs': [1, 2]},
-#     'VGGNet': {'losses': [0.5293657882160611, 0.16392043011718327], 'epochs': [1, 2]},
-#     'DenseNet': {'losses': [0.6854921842310163, 0.11557560107575522], 'epochs': [1, 2]}
-# }
+import os
+from datetime import datetime
 
 def plot_losses(results, val_results):
+    os.makedirs("plots", exist_ok=True)
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     plt.figure(figsize=(10, 6))
     for model_name, data in results.items():
         # available flags for customizing: linestyle="--", linewidth=2, marker,
@@ -21,9 +16,12 @@ def plot_losses(results, val_results):
     plt.title('Losses per Epoch')
     plt.legend()
     plt.grid(True, linestyle='--', alpha=0.5)
+    plt.savefig(os.path.join("plots", f'losses_plot_{now}.png'))
     plt.show()
 
 def plot_accuracies(results, val_results):
+    os.makedirs("plots", exist_ok=True)
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     plt.figure(figsize=(10, 6))
     for model_name, data in results.items():
         plt.plot(data['epochs'], data['accuracies'], label=model_name)
@@ -34,4 +32,5 @@ def plot_accuracies(results, val_results):
     plt.title('Accuracy per Epoch')
     plt.legend()
     plt.grid(True, linestyle='--', alpha=0.5)
+    plt.savefig(os.path.join("plots", f'accuracies_plot_{now}.png'))
     plt.show()
