4c3c9d0a5b3e65433814fddf507a5a324e851b1d
ByloTonix <bylotonix@gmail.com>
2024-05-06 22:59:30 +0300

    one more model

---
+++ models/resnet18.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class ResNet18(nn.Module):
+    def __init__(self, num_classes=1):
+        super(ResNet18, self).__init__()
+        self.resnet = models.resnet18(pretrained=True)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Sequential(
+            nn.Linear(num_ftrs, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(512, num_classes),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.resnet(x)
+        return x
+
+def load_model(num_classes=1):
+    model = ResNet18(num_classes=num_classes)
+    return model
+
