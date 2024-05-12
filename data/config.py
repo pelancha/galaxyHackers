9d7b1d019946c6a667c94fdced9e2a0daf35bd65
pelancha <sobachekrokky@gmail.com>
2024-05-12 16:29:16 +0300

    update config

--- data/config.py
+++ data/config.py
@@ -6,8 +6,16 @@ mapACT = "act_planck_dr5.01_s08s18_AA_f220_daynight_fullivar.fits"
 dr5_clusters = "DR5_cluster-catalog_v1.1.fits"
 archieve_dr5 = "https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/"
 
+'''Links to download collected data are available until August, 10'''
+act_wget = "http://oshi.at/JTuj"
+dr5_wget = "http://oshi.at/ysSg"
+data_wget = "http://oshi.at/GtZB"
+
 mapACT_url = archieve_dr5 + 'maps/' + mapACT
 dr5_clusters_url = archieve_dr5 + dr5_clusters
+
+zipped_act_out = subpath + "ACT.zip"
+zipped_dr5_out = subpath + "dr5.zip"
 
 mapACT_out = subpath + mapACT
 dr5_clusters_out = subpath + dr5_clusters
@@ -21,8 +29,7 @@ randoms_out = segmentation_maps_pics + "r5.csv"
 stars_out = segmentation_maps_pics + "gaia5.csv"
 
 data_out = working_path + "data/"
-'''Link to download ddosed data for dataloaders is available until August, 10'''
-data_wget = "http://oshi.at/GtZB"
+
 zipped_data_out = data_out + "DATA.zip"
 
 seg_maps = "segmentation_maps/"
