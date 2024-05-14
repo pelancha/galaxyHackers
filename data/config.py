ebc93a42ad17e9fc9d581a8105bb1f40e2a600c8
pelancha <sobachekrokky@gmail.com>
2024-05-14 03:11:26 +0300

    update saveBigSegMap()

--- data/config.py
+++ data/config.py
@@ -6,13 +6,15 @@ mapACT = "act_planck_dr5.01_s08s18_AA_f220_daynight_fullivar.fits"
 dr5_clusters = "DR5_cluster-catalog_v1.1.fits"
 archieve_dr5 = "https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr5/"
 
+mapACT_url = archieve_dr5 + 'maps/' + mapACT
+dr5_clusters_url = archieve_dr5 + dr5_clusters
+
 '''Links to download collected data are available until August, 10'''
 act_wget = "http://oshi.at/JTuj"
 dr5_wget = "http://oshi.at/ysSg"
 data_wget = "http://oshi.at/GtZB"
-
-mapACT_url = archieve_dr5 + 'maps/' + mapACT
-dr5_clusters_url = archieve_dr5 + dr5_clusters
+example_wget = "http://oshi.at/bJwn"
+bigSegMap_wget = "http://oshi.at/pfXj"
 
 zipped_act_out = subpath + "ACT.zip"
 zipped_dr5_out = subpath + "dr5.zip"
@@ -20,16 +22,23 @@ zipped_dr5_out = subpath + "dr5.zip"
 mapACT_out = subpath + mapACT
 dr5_clusters_out = subpath + dr5_clusters
 
-segmentation_maps_pics = working_path + "data/example/"
+data_out = working_path + "data/"
+
+zipped_data_out = data_out + "DATA.zip"
+zipped_example_out = data_out + "example.zip"
+
+segmentation_maps_pics = data_out + "example/"
 samples_location = working_path + location
 dr5_sample_location = samples_location + "test_dr5/"
+madcows_sample_location = samples_location + "test_madcows/"
+gaia_sample_location = samples_location + "test_gaia/"
 
 clusters_out = segmentation_maps_pics + "cl5.csv"
 randoms_out = segmentation_maps_pics + "r5.csv"
 stars_out = segmentation_maps_pics + "gaia5.csv"
 
-data_out = working_path + "data/"
-
-zipped_data_out = data_out + "DATA.zip"
-
 seg_maps = "segmentation_maps/"
+bigSegMapLocation = data_out + "example_big/"
+bigSegMap_pics = bigSegMapLocation + "Big/"
+cl_bigSegMap_out = bigSegMapLocation + "cluster_bigSegMap.csv"
+zipped_bigSegMap_out = data_out + "example_big.zip"
--- data/segmentation.py
+++ data/segmentation.py
@@ -14,6 +14,8 @@ import matplotlib as mpl
 from matplotlib import cm
 import data.data as data
 from data.config import *
+from zipfile import ZipFile 
+import wget
 
 
 class ImageSet(Dataset):
@@ -42,10 +44,11 @@ class ImageSet(Dataset):
 
 def prepare_gaia():
     data_gaia = data.read_gaia()
-    path = samples_location + 'test_gaia'
-    os.makedirs(path, exist_ok=True)
-
-    legacy_for_img.grab_cutouts(target_file=data_gaia, output_dir=path,
+# gaia might have already been collected with the rest of data during dataloaders creation
+    if (not os.path.exists(gaia_sample_location) or 
+        len(os.listdir(gaia_sample_location)) == 0):
+      os.makedirs(gaia_sample_location, exist_ok=True)
+      legacy_for_img.grab_cutouts(target_file=data_gaia, output_dir=gaia_sample_location,
                                           survey='unwise-neo7', imgsize_pix = 224*8, file_format='jpg' )
     return data_gaia
 
@@ -78,6 +81,21 @@ def predict_folder(folder, model, optimizer_name, device='cuda:0'):
 
 def predict_tests(model, optimizer_name):
     test_dr_0, test_dr_1 = data.train_val_test_split()[2]
+    if (not os.path.exists(location) or 
+        len(os.listdir(f'{madcows_sample_location}1')) == 0):
+        os.makedirs(data_out, exist_ok=True)
+        try:
+            wget.download(url=data_wget, out=data_out)
+            with ZipFile(f"{zipped_data_out}", 'r') as zObject: 
+                zObject.extractall(path=f"{data_out}")
+            os.remove(f"{zipped_data_out}")
+        except:
+            data.ddos()
+        else:
+            if (not os.path.exists(location) or 
+                    len(os.listdir(location)) == 0):
+                data.ddos()
+
     gaia = prepare_gaia()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -110,7 +128,7 @@ def create_samples(model, optimizer_name):
         os.makedirs(path, exist_ok=True)
 
     samples = predict_tests(model, optimizer_name)
-    id = 0
+    samples5_final = []
     for test in samples:
         sample5 = test.sample(5, random_state=5).reset_index(drop=True)
         max_ra = sample5['RA'].max()
@@ -120,14 +138,11 @@ def create_samples(model, optimizer_name):
             sample5 = test.sample(5).reset_index(drop=True)
             max_ra = sample5['RA'].max()
             max_de = sample5['DEC'].max()
-        match id:
-            case 0:
-                sample5.to_csv(clusters_out, index=False)
-            case 1:
-                sample5.to_csv(randoms_out, index=False)
-            case 2:
-                sample5.to_csv(stars_out, index=False)
-        id += 1
+        samples5_final.append(sample5)
+
+    samples5_final[0].to_csv(clusters_out, index=False)
+    samples5_final[1].to_csv(randoms_out, index=False)
+    samples5_final[2].to_csv(stars_out, index=False)
 
 
 def createSegMap(id, ra0, dec0, name, dire): #id: 0 for small segmentation maps, 1 - for a big one
@@ -169,24 +184,45 @@ def createSegMap(id, ra0, dec0, name, dire): #id: 0 for small segmentation maps,
     # print(data)
     # return data.shape
 
+'''Function to create segmentation maps for chosen samples'''
 
-def formSegmentationMaps(model, optimizer_name):
-    create_samples(model, optimizer_name) #returns csvs
+def prepare_samples():
     cl5 = pd.read_csv(clusters_out)
     r5 =  pd.read_csv(randoms_out)
     gaia5 = pd.read_csv(stars_out)
-
     all_samples = [("Clusters", cl5), ("Random", r5), ("Stars", gaia5)]
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
     for i in range(5):
         createSegMap(0, all_samples[0][1].loc[i, 'RA'], all_samples[0][1].loc[i, 'DEC'], all_samples[0][1].loc[i, 'Component_name'], dire = f'{segmentation_maps_pics}Cl/{i}')
         createSegMap(0, all_samples[1][1].loc[i, 'RA'], all_samples[1][1].loc[i, 'DEC'], all_samples[1][1].loc[i, 'Component_name'], dire = f'{segmentation_maps_pics}R/{i}')
         createSegMap(0, all_samples[2][1].loc[i, 'RA'], all_samples[2][1].loc[i, 'DEC'], all_samples[2][1].loc[i, 'Component_name'], dire = f'{segmentation_maps_pics}GAIA/{i}')
+    return all_samples
+
+
+def formSegmentationMaps(model, optimizer_name):
+    if (not os.path.exists(segmentation_maps_pics) or 
+        len(os.listdir(segmentation_maps_pics)) == 0):
+        os.makedirs(data_out, exist_ok=True)
+        try:
+            wget.download(url=example_wget, out=data_out)
+            with ZipFile(f"{zipped_example_out}", 'r') as zObject: 
+                zObject.extractall(path=f"{data_out}")
+            os.remove(f"{zipped_example_out}")
+            cl5 = pd.read_csv(clusters_out)
+            r5 =  pd.read_csv(randoms_out)
+            gaia5 = pd.read_csv(stars_out)
+            all_samples = [("Clusters", cl5), ("Random", r5), ("Stars", gaia5)]
+        except:
+            create_samples(model, optimizer_name) #returns csvs
+            all_samples = prepare_samples()
+        else:
+            if (not os.path.exists(segmentation_maps_pics) or 
+                len(os.listdir(segmentation_maps_pics)) == 0):
+                create_samples(model, optimizer_name) #returns csvs
+                all_samples = prepare_samples()
 
     prob_clusters, prob_randoms, prob_gaia = [], [], []
 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     for i in range(5):
         prob_clusters.append(predict_folder(f'{segmentation_maps_pics}Cl/{i}', model, optimizer_name, device=device))
         prob_randoms.append(predict_folder(f'{segmentation_maps_pics}R/{i}', model, optimizer_name, device=device))
@@ -194,6 +230,7 @@ def formSegmentationMaps(model, optimizer_name):
 
     predictions = [prob_clusters, prob_randoms, prob_gaia]
     return all_samples, predictions
+
 
 def saveSegMaps(selected_models, optimizer_name):
     for model_name, model in selected_models:
@@ -210,30 +247,75 @@ def saveSegMaps(selected_models, optimizer_name):
             plt.savefig(f"{working_path}{seg_maps}{model_name}_{optimizer_name}_{all_samples[i][0]}.png")
             plt.close()
 
-def saveBigSegMap(selected_models, optimizer_name):
-    test_dr5, test_madcows = data.train_val_test_split()[2:4]
-    test_dr5_0, test_dr5_1 = test_dr5
-    test_madcows_0, test_madcows_1 = test_madcows
-    df = pd.concat([test_dr5_0, test_dr5_1, test_madcows_0, test_madcows_1], ignore_index=True)
+'''Functions to create a segmentation map in a 30x30 box for a cluster with step 1 minute'''
 
-    cl0 = df[df.target==1].sample(1, random_state=1).reset_index(drop=True)
+def create_sample_big(test_dr5_1, test_madcows_1):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    if (not os.path.exists(location) or 
+        len(os.listdir(f'{madcows_sample_location}1')) == 0):
+        os.makedirs(data_out, exist_ok=True)
+        try:
+            wget.download(url=data_wget, out=data_out)
+            with ZipFile(f"{zipped_data_out}", 'r') as zObject: 
+                zObject.extractall(path=f"{data_out}")
+            os.remove(f"{zipped_data_out}")
+        except:
+            data.ddos()
+        else:
+            if (not os.path.exists(location) or 
+                    len(os.listdir(location)) == 0):
+                data.ddos()
+    test_dr5_1['prob'] = predict_folder(f'{dr5_sample_location}1', model, device=device)
+    test_madcows_1['prob'] = predict_folder(f'{madcows_sample_location}1', model, device=device)
+    df = pd.concat([test_dr5_1, test_madcows_1], ignore_index=True)
+
+    cl0 = df.sample(1, random_state=1).reset_index(drop=True)
     max_ra = cl0['RA'].max()
     max_de = cl0['DEC'].max()
     required_space = 15 / 60 #15 minutes including shift
     while (max_ra + required_space) > 360 or (max_de - required_space) < -90:
-        cl0 = df[df.target==1].sample(1, random_state=1).reset_index(drop=True)
+        cl0 = df.sample(1).reset_index(drop=True)
         max_ra = cl0['RA'].max()
         max_de = cl0['DEC'].max()
-
-    bigSegMapLocation = f'{segmentation_maps_pics}Big'
     os.makedirs(bigSegMapLocation, exist_ok=True)
+    cl0.to_csv(cl_bigSegMap_out, index=False)
+
+    os.makedirs(bigSegMap_pics, exist_ok=True)
+    createSegMap(1, cl0.loc[0, 'RA'], cl0.loc[0, 'DEC'], cl0.loc[0, 'Component_name'], dire=bigSegMap_pics)
+
+
+def saveBigSegMap(selected_models, optimizer_name):
+    if (not os.path.exists(bigSegMapLocation) or 
+        len(os.listdir(bigSegMap_pics)) == 0):
+        os.makedirs(data_out, exist_ok=True)
+        try:
+            wget.download(url=bigSegMap_wget, out=data_out)
+            with ZipFile(f"{zipped_bigSegMap_out}", 'r') as zObject: 
+                zObject.extractall(path=f"{data_out}")
+            os.remove(f"{zipped_bigSegMap_out}")
+            cl0 = pd.read_csv(cl_bigSegMap_out)
+        except:
+            test_dr5, test_madcows = data.train_val_test_split()[2:4]
+            test_dr5_0, test_dr5_1 = test_dr5
+            test_madcows_0, test_madcows_1 = test_madcows
+            create_sample_big(test_dr5_1, test_madcows_1)
+            cl0 = pd.read_csv(cl_bigSegMap_out)
+        else:
+            if (not os.path.exists(bigSegMapLocation) or 
+                len(os.listdir(bigSegMap_pics)) == 0):
+                test_dr5, test_madcows = data.train_val_test_split()[2:4]
+                test_dr5_0, test_dr5_1 = test_dr5
+                test_madcows_0, test_madcows_1 = test_madcows
+                create_sample_big(test_dr5_1, test_madcows_1)
+                # cl0 = pd.read_csv(cl_bigSegMap_out)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+    cl0 = pd.read_csv(cl_bigSegMap_out)
     for model_name, model in selected_models:
-        createSegMap(1, cl0.loc[0, 'RA'], cl0.loc[0, 'DEC'], cl0.loc[0, 'Component_name'], dire=bigSegMapLocation)
-        prob_big = predict_folder(bigSegMapLocation, model, optimizer_name, device=device)
+        prob_big = predict_folder(bigSegMap_pics, model, optimizer_name, device=device)
         plt.imshow(prob_big.reshape(30, 30), cmap=cm.PuBu)
+        plt.title("{:.4f}".format(cl0.loc[0, "prob"]))
         plt.axis('off')
         os.makedirs(seg_maps, exist_ok=True)
         plt.savefig(f"{working_path}{seg_maps}{model_name}_{optimizer_name}_Big.png")
