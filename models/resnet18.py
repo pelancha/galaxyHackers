7e66b30be377a347b5f4ef78766b2df7d8a3b166
Matvei Zekhov <zehov1@mail.ru>
2024-08-10 15:41:28 +0300

    migrated to 2-channel fits data

--- models/resnet18.py
+++ models/resnet18.py
@@ -6,6 +6,17 @@ class ResNet18(nn.Module):
     def __init__(self, num_classes=2):
         super(ResNet18, self).__init__()
         self.resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+
+        # Cloning pretrained weights of old layer
+        weight = self.resnet.conv1.weight.clone()
+
+        # Defining new layer
+        self.resnet.conv1 = nn.Conv2d(2, self.resnet.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+
+        # Inserting pretrained weights from first 2 channels into new layer
+        with torch.no_grad():
+            self.resnet.conv1.weight = nn.Parameter(weight[:, :2])
+
         num_ftrs = self.resnet.fc.in_features
         self.resnet.fc = nn.Sequential(
             nn.Linear(num_ftrs, 512),
--- models/vitL16.py
+++ models/vitL16.py
@@ -17,4 +17,14 @@ def load_model():
     layer_width = 512
     model_ft = timm.create_model('vit_large_patch16_224', pretrained=True, num_classes = 2)
     model_ft.head = Sigm(num_classes=2)
+
+
+    pretrained_weights = model_ft.patch_embed.proj.weight.clone()
+
+    model_ft.patch_embed.proj = nn.Conv2d(2, 1024, kernel_size=(16, 16), stride=(16, 16))
+
+    # Inserting pretrained weights from first 2 channels into new layer
+    with torch.no_grad():
+        model_ft.patch_embed.proj.weight.data = nn.Parameter(pretrained_weights[:, :2])
+        
     return model_ft
